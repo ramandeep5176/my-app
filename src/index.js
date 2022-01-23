1da39a4a91841844638c597013006814351bebde
ramandeep5176 <ramandeepgill5176@gmail.com>
2022-01-22 19:30:17 -0500

    react22

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//
+//
+import reactDom from "react-dom";
+import Test from "./test.js";
+function One() {
+  return (
+    <>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias
+        repellendus obcaecati ut eos ea, unde debitis molestiae velit, veniam
+        labore no bis hic voluptatum dolor? Voluptatibus laudantium amet tenetur
+        atque libero.
+      </h1>
+      this is raw
+      <p>raman</p>
+      <Test />
+    </>
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reactDom.render(<One />, document.getElementById("root"));
---
+++ src/test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const test = () => {
+  return <>
+  <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, quod.</h1>
+  </>;
+};
+
+export default test;
+
+
